8d308bd74a72d93c51ce5fc4b126e7196c184da4
Mukul Jain <mukul.jain1906@yahoo.com>
2023-09-28 10:24:02 +0530

    Added locate function

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # WSN-Localisation-Models
+
+This repository contains various Optimisation Models to optimise SVR Parameters for Localistion in a Wireless Sensor Network.
+
+### Installation
+`pip install -r requirements.txt`
+
+### Execution
+1. To change the Hyperparameters, go to `config` file in `settings` directory
+
+2. To run
+ `python {Model}.py settings/{Model.config}`
+
+3. Models are saved in the `models` directory as `pickle` files
+
+
+
--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+
 def load_data(data_config):
     """
     Reads data from a csv file and returns X and Y separately.
@@ -8,6 +9,29 @@ def load_data(data_config):
     feature_cols = [i for i in range(int(data_config["feature_cols"]))]
     label_col = int(data_config["label_col"])
 
-    data = np.genfromtxt(data_file, delimiter = ",")
+    data = np.genfromtxt(data_file, delimiter=",")
 
-    return data[:,feature_cols], data[:,label_col]
+    return data[:, feature_cols], data[:, label_col]
+
+
+def locate(coordinates, angles, distance):
+    """
+    Takes the following as parameters:
+    - coordinates of the sensor
+    - angles observed by the sensor (in degrees)
+    - distance measured using RSSI values
+
+    Returns:
+    - coordinates of the object
+
+    Refer:
+    https://www.geogebra.org/m/yj2sVvNJ
+    """
+    assert len(coordinates) == 3
+    assert len(angles) == 3
+
+    position = []
+    for i in range(len(coordinates)):
+        position.append(coordinates[i] + distance * np.cos(np.deg2rad(angles[i])))
+
+    return position
